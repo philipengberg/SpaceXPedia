e8f99189d82f96c9215d51077a59b31628557503
Philip Engberg <philipengberg@gmail.com>
2018-08-20 16:08:14 +0200

    Initial Commit

---
+++ SpaceX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpaceX
+//
+//  Created by Philip Engberg on 20/08/2018.
+//  Copyright © 2018 Simple Sense. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
